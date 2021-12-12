87075243c6f5a4e0d77297be33f1fc2253fb467b
agvenmuharisrifqi <agven25@gmail.com>
2021-12-13 00:51:11 +0700

    Initial Ternak Kambing

---
+++ js/themeJS.js
@@ -0,0 +1,39 @@
+$(function() {
+    $(".navbar-toggler").on("click", function(e) {
+        $(".tm-header").toggleClass("show");
+        e.stopPropagation();
+    });
+    
+      $("html").click(function(e) {
+        var header = document.getElementById("tm-header");
+    
+        if (!header.contains(e.target)) {
+          $(".tm-header").removeClass("show");
+        }
+      });
+    
+      $("#tm-nav .nav-link").click(function(e) {
+        $(".tm-header").removeClass("show");
+      });
+});
+
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function sectionNav(e){
+  document.body.scrollTop = e;
+  document.documentElement.scrollTop = e;
+}
